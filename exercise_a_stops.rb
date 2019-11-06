4aaddacffe9e20bb2c93674e40b3e21464f18886
Gregor Lennie <gregorlennie@hotmail.co.uk>
2019-11-06 18:20:51 +0000

    Adds solutions for Exercise A

--- exercise_a_stops.rb
+++ exercise_a_stops.rb
@@ -16,13 +16,20 @@ stops.index("Linlithgow")
 stops.delete("Livingston")
 
 # 6. Delete `"Cumbernauld"` from the array by index
+stops.delete_at(2)
 
 # 7. How many stops there are in the array?
+stops.length
 
 # 8. How many ways can we return `"Falkirk High"` from the array?
+stops[2]
+stops.at(2)
+stops.fetch(2)
 
 # 9. Reverse the positions of the stops in the array
+stops.reverse
 
 # 10. Print out all the stops using a for loop
-
-p stops
+for stop in stops
+  p stop
+end
